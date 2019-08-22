f07ad979bb8831b5c898c079bc7d554837d3c88b
8bitjustin <j.olson.digital@gmail.com>
2019-08-22 11:02:49 -0700

    Removed photo.jpg, added BR_Selfie_Resize.jpg. Added code to simply display in grayscale.

--- face_detector.py
+++ face_detector.py
@@ -2,3 +2,9 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+img = cv2.imread("BR_Selfie_Resize.jpg")
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Gray", img_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
