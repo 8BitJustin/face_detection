4e095217f3a90a46e85f4c9c16a80f3ffbbc7e7f
8bitjustin <j.olson.digital@gmail.com>
2019-08-22 17:19:17 -0700

    Added for loop for img to create coordinates and border box color.

--- face_detector.py
+++ face_detector.py
@@ -5,6 +5,16 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 img = cv2.imread("BR_Selfie_Resize.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Gray", img_gray)
+faces = face_cascade.detectMultiScale(img_gray,
+                                      scaleFactor=1.05,
+                                      minNeighbors=5)
+
+for x, y, w, h in faces:
+    img = cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 3)
+
+print(type(faces))
+print(faces)
+
+cv2.imshow("Gray", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
